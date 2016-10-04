7aaef10ee9ab32e302376bc7c27ada55c51160db
atolat <atolat@uci.edu>
2016-10-04 08:08:37 -0700

    Cleaned code, custom classes work!

--- integration-ca-world.js
+++ integration-ca-world.js
@@ -34,9 +34,6 @@ window._wfx_settings.onBeforeShow = function (event) {
 
 
 
-document.getElementsByTagName('body').onload = function () {
-    window.alert('loaded');
-}
 
 
 //Setting up the object for adding buttons at page level.
@@ -52,24 +49,40 @@ window._wfx_settings.apply_page_settings = function () {
     $.each(window._wfx_settings['page_settings'], function (name, settings) {
         if (window.location.hash.indexOf(settings.hash) != -1) {
             window._wfx_settings.init_page(name, settings);
-
             if (settings.self_help) {
                 self_help_applied = true;
             }
         }
     });
-    
+
+
+    //Portfolios flow.
     //Adding custom classes to elements...
-    if(window.location.hash.startsWith("#action:pfm.portfolioList")){
-      $('[onclick=\"navigateToURL(\'nu?action=npt.setObjectUserPartitions\',\'navFromActionId=pfm.portfolioList\',\'navToActionId=pfm.portfolioCreate\',\'objectCode=pfm_portfolio\',\'ui.page.space=pfm.portfolios\');\"]').addClass("new_one");
+    if (window.location.hash.startsWith("#action:pfm.portfolioList")) {
+        //window.alert('Event step is::'+event.step);
+        $('[onclick=\"navigateToURL(\'nu?action=npt.setObjectUserPartitions\',\'navFromActionId=pfm.portfolioList\',\'navToActionId=pfm.portfolioCreate\',\'objectCode=pfm_portfolio\',\'ui.page.space=pfm.portfolios\');\"]').addClass("new_one");
     }
-    
-    if($('[title="Create Portfolio"]').length && window.location.hash.startsWith("#action:pfm.portfolioCreate")){
+
+    if ($('[title="Create Portfolio"]').length && window.location.hash.startsWith("#action:pfm.portfolioCreate")) {
         $('[onclick=\"optionSelectAll(\'pagex\',\'owner\');optionSelectAll(\'pagex\',\'stakeholder\');submitForm(\'pagex\',\'pfm.portfolioCreateSave\',\'redirect_action=pfm.portfolioProperties\');\"]').addClass("portfolio_save");
-        
+
         $('[onclick*="pfm.portfolioCreateSubmit"]').addClass("portfolio_save_return");
     }
+
+
+    //Change Request Flow
+    if (window.location.hash.startsWith("#action:projmgr") && $('[title="Project Indicators"]').length && $('h1[title*="Dashboard"]').length) {
+        $('[title="Risks/Issues/Changes"]').addClass("project_ric_click");
+    }
+
+    if (window.location.hash.startsWith("#action:itl.riskList") && $('h1[title*="Project"]').length) {
+        $('[title="Risks/Issues/Changes"]').addClass("project_ric_hover");
+    }
     
+    if (window.location.hash.includes("changeList")){
+        $('[onclick*="action=itl.changeObject"]').addClass("changereq_new");
+    }
+
     // Relook for buttons periodically as PPM is deleting buttons at times.
     window.setTimeout(function () {
         window._wfx_settings.apply_page_settings();
@@ -86,47 +99,16 @@ window._wfx_settings.apply_page_settings();
 window._wfx_settings['203a5610-7f64-11e6-9479-04013d24cd02'] = function (event) {
     var potential_step;
 
-    //Adding custom classes to elements
-    // "New Button"
     if (window.location.hash.startsWith("#action:pfm.portfolioList")) {
         potential_step = 2;
 
-    }
-
-    //    //This section is for restarting the walkthrough relative to current location.	
-    //    if (window.location.hash.includes("action:pma.ideaList")) {
-    //        potential_step = 2;
-    //
-    //
-    //    }
-    //    if (window.location.hash.startsWith("#action:pma.ideaCreate")) {
-    //        potential_step = 3;
-    //
-    //    }
-
+   
 
     if (potential_step && event.step < potential_step) {
         return {
             "position": potential_step
         };
     }
-
-    //  window.alert('outside if');
-    //  
-    //		  if( ( window.location.hash.includes("#action:projmgr.projectDefaultTab")  &&($("label")[1].innerHTML) == 'Project ID')
-    //			|| (window.location.hash.includes("#action:projmgr.projectDefaultTab") &&  $('h2[title="Project Indicators"]').length >0))
-    //		  {
-    //		  window.alert('inside of if add class');
-    //		  sleep(100000);
-    //		  $('[title="Risks/Issues/Changes"]').addClass('abc');
-    //		  
-    //		  window.alert( $(".abc"));
-    //		  window.alert('in the end');
-    //		  }
-    //  
-    //  // $('[title="Risks/Issues/Changes"]').addClass('abc');
-
-
 
 
 
